31369baf0ea45f65a511dc2e81040440cb0b72bb
Asaf s <playgik012@gmail.com>
2021-07-13 03:14:38 +0300

    Second file updated

--- tic_tac_toe_game.c
+++ tic_tac_toe_game.c
@@ -13,7 +13,7 @@ void print_x_o_table(char loc[3][3])
 
 }
 
-/* func that check if was a win. */
+/* Func that check if was a win. */
 char win(char a, char b, char c)
 {
     if (a != ' ' && b != ' ' && c != ' ')
@@ -32,6 +32,38 @@ char win(char a, char b, char c)
     }
 }
 
+/* Checking if was 9 steps in the game and wasn't win */
+void equal(int a)
+{
+    if (a == 9)
+    {
+        printf("EQUAL GAME!\n");
+        game_continue = 1; //Game over
+    }
+}
+
+/* Checking who turn and Change turn */
+char who_turn(char turn)
+{
+    if (turn == 'x')
+        {
+            return 'o';
+        }
+    else
+        {
+            return 'x';
+        }
+}
+
+int is_a_number()
+{
+    int user_input;
+    if (scanf("%d", &user_input) == 1) //checking if user input is a number.
+    {
+        return user_input;
+    }
+}
+
 int main()
 {
     char table[3][3] = {
@@ -39,48 +71,51 @@ int main()
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     };
+
     char turn_of = 'x'; // x is start in the game
-    int pointer; // user input
     int table_chnages = 0; // Some changes were made
 
     while(game_continue == 0)
     {
         print_x_o_table(table);
-        printf("Now is turn of: %c, choose a slot between 1 - 9: ", turn_of);
-        scanf("%d", &pointer);
+        printf("\nNow is turn of: %c, choose a slot between 1 - 9: ", turn_of);
 
-        // checking if user input is a number it the ranage.
-        if (pointer < 0 || pointer > 9) //|| table[pointer / 3][pointer % 3 - 1] != ' ')
-        {
-            printf("This number out of range!\n");
-            continue;
-        }
-        if (table[pointer / 3][pointer % 3 - 1] == ' ')
-        {
-            table[pointer / 3][pointer % 3 - 1] = turn_of;
-            table_chnages += 1;
-            if (table_chnages == 9)
+        // if (scanf("%d", &user_input) == 1) //checking if user input is a number.
+        // {
+            int user_input = is_a_number(); // user input
+            if (0 < user_input && user_input < 10) // checking if user input is a number it the ranage.
             {
-                printf("EQUAL GAME!\n");
-                game_continue = 1; //Game over
+                if (table[user_input / 3][user_input % 3 - 1] == ' ')
+                {
+                    table[user_input / 3][user_input % 3 - 1] = turn_of;
+                    table_chnages += 1; // If table_change == 9 the game will wover' that mean was 9 turn.
+                    turn_of = who_turn(turn_of); // change user turn
+                }
+                else
+                {
+                    printf("\nChoose another number, this slot is occupied.\n");
+                    // continue;
+                }
             }
-        }
-        else
-        {
-            continue;
-        }
+            else
+            {
+                printf("\nThis number out of the range!\n");
+                // continue;
+            }
+        /*}
 
-        // change turn user
-        if (turn_of == 'x')
-            {
-                turn_of = 'o';
-            }
-        else
-            {
-                turn_of = 'x';
-            }
 
-        // // checking if was a win
+        if user // If user input is not a number.
+        {
+            printf("\nPlease enter number only.\n");
+        }
+        */
+
+
+        /* Checking if game equal */
+        equal(table_chnages);
+
+        /* Checking if was a win */
         win(table[0][0], table[0][1], table[0][2]); // line number 1 is equal.
         win(table[1][0], table[1][1], table[1][2]); // line number 2 is equal.
         win(table[2][0], table[2][1], table[2][2]); // line number 3 is equal.
@@ -94,5 +129,6 @@ int main()
     }
 
     print_x_o_table(table); // Print table when game over.
+    getch();
     return 0;
 }
